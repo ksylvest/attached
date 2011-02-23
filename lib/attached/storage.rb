960e92624a2e55bdd41bcca8bd2b310c89314fb4
Kevin Sylvestre <kevin@ksylvest.com>
2011-02-23 11:57:34 -0500

    added support for local storage and cleaned up documentation

--- lib/attached/storage.rb
+++ lib/attached/storage.rb
@@ -1,6 +1,8 @@
+require 'attached/storage/base'
 require 'attached/storage/aws'
 require 'attached/storage/google'
 require 'attached/storage/rackspace'
+require 'attached/storage/local'
 
 module Attached
   module Storage
@@ -10,17 +12,22 @@ module Attached
     #
     # Usage:
     #
+    #   Attached::Storage.storage(:local)
     #   Attached::Storage.storage(:aws,       "#{Rails.root}/config/aws.yml"      )
     #   Attached::Storage.storage(:google,    "#{Rails.root}/config/google.yml"   )
     #   Attached::Storage.storage(:rackspace, "#{Rails.root}/config/rackspace.yml")
+    #   Attached::Storage.storage(Attached::Storage::Custom.new)
     
-    def self.storage(medium, credentials)
+    def self.storage(storage, credentials)
       
-      case medium
+      return storage if storage.is_a? Attached::Storage::Base
+      
+      case storage
         when :aws       then return Attached::Storage::AWS.new       credentials
         when :google    then return Attached::Storage::Google.new    credentials
         when :rackspace then return Attached::Storage::Rackspace.new credentials
-        else raise "undefined storage medium '#{medium}'"
+        when :local     then return Attached::Storage::Local.new
+        else raise "undefined storage '#{storage}'"
       end
       
     end
