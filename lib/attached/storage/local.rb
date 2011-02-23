c28466356873818dd33e83c9179adfc36e93b5e2
Kevin Sylvestre <kevin@ksylvest.com>
2011-02-23 11:57:08 -0500

    added support for a local storage specifically for testing

---
+++ lib/attached/storage/local.rb
@@ -0,0 +1,74 @@
+require 'attached/storage/base'
+
+module Attached
+	module Storage
+		class Local < Base
+			
+			
+			attr_reader :mode
+			
+			
+			# Create a new interface supporting save and destroy operations.
+			#
+			# Usage:
+			#
+			#		Attached::Storage::Local.new()
+			
+			def initialize()
+				@mode = 0644
+			end
+			
+			
+			# Access the host (e.g. https://attached.commondatastorage.googleapis.com/) for a storage service.
+			#
+			# Usage:
+			#
+			#		storage.host
+			
+			def host()
+			  "/system/"
+			end
+			
+			
+      # Save a file to a given path.
+      #
+      # Parameters:
+      #
+      # * file - The file to save.
+      # * path - The path to save.
+      
+      def save(file, path)
+        path = "#{Rails.root}/public/system/#{path}"
+        dirname, basename = File.split(path)
+        
+        file.close
+        
+        begin
+          FileUtils.mkdir_p(dirname)
+          FileUtils.mv(file.path, path)
+          FileUtils.chmod(self.mode, path)
+        rescue Errno::ENOENT
+        end
+      end
+      
+      
+      # Destroy a file at a given path.
+      #
+      # Parameters:
+      #
+      # * path - The path to destroy.
+      
+      def destroy(path)
+        path = "#{Rails.root}/public/system/#{path}"
+        dirname, basename = File.split(path)
+    
+        begin
+          FileUtils.rm(path)
+        rescue Errno::ENOENT
+        end
+      end
+      
+			
+		end
+	end
+end
