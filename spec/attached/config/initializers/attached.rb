afc75be52996634b89fcc1b66b866246e257ff82
Kevin Sylvestre <kevin@ksylvest.com>
2011-02-23 11:58:55 -0500

    removed aws default and added placeholder for other providers

--- spec/attached/config/initializers/attached.rb
+++ spec/attached/config/initializers/attached.rb
@@ -1,2 +1,8 @@
-Attached::Attachment.options[:medium] = :aws
-Attached::Attachment.options[:credentials] = "#{Rails.root}/config/aws.yml"
+# Attached::Attachment.options[:medium] = :aws
+# Attached::Attachment.options[:credentials] = "#{Rails.root}/config/aws.yml"
+
+# Attached::Attachment.options[:medium] = :google
+# Attached::Attachment.options[:credentials] = "#{Rails.root}/config/google.yml"
+
+# Attached::Attachment.options[:medium] = :rackspace
+# Attached::Attachment.options[:credentials] = "#{Rails.root}/config/rackspace.yml"
