c3ecff8c9e26ec1003fc4c9a2bec6bed23605ffc
Kevin Sylvestre <kevin@ksylvest.com>
2011-03-07 13:36:23 -0500

    added environmental variable test

--- test/config/initializers/attached.rb
+++ test/config/initializers/attached.rb
@@ -1,8 +1,11 @@
-# Attached::Attachment.options[:medium] = :aws
-# Attached::Attachment.options[:credentials] = "#{Rails.root}/config/aws.yml"
-
-# Attached::Attachment.options[:medium] = :google
-# Attached::Attachment.options[:credentials] = "#{Rails.root}/config/google.yml"
-
-# Attached::Attachment.options[:medium] = :rackspace
-# Attached::Attachment.options[:credentials] = "#{Rails.root}/config/rackspace.yml"
+case ENV['ATTACHED']
+when "aws"
+  Attached::Attachment.options[:medium] = :aws
+  Attached::Attachment.options[:credentials] = "#{Rails.root}/config/aws.yml"
+when "google"
+  Attached::Attachment.options[:medium] = :google
+  Attached::Attachment.options[:credentials] = "#{Rails.root}/config/google.yml"
+when "rackspace"
+  Attached::Attachment.options[:medium] = :rackspace
+  Attached::Attachment.options[:credentials] = "#{Rails.root}/config/rackspace.yml"
+end
