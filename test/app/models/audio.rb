48626ae40f76097d36005224e69cdd590320fbda
Kevin Sylvestre <kevin@ksylvest.com>
2011-03-07 14:59:06 -0500

    updated test for path testing

--- test/app/models/audio.rb
+++ test/app/models/audio.rb
@@ -1,11 +1,13 @@
 class Audio < ActiveRecord::Base
   
-  has_attached :file, :processor => :audio, :styles => {
+  has_attached :file, :processor => :audio, :path => "audios/files/:style/:identifier:extension", 
+  :styles => {
     :small => { :preset => "128kbps" },
     :large => { :preset => "256kbps" },
   }
   
   validates_attached_presence :file
   validates_attached_size :file, :in => 2.kilobytes..2.megabytes
+  validates_attached_extension :file, :in => %w(wav aif)
   
 end
