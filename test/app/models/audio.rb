cf188fc6ff850b9b834abb3102a88dac4e8b3e07
Kevin Sylvestre <kevin@ksylvest.com>
2011-05-15 14:58:54 -0400

    updated spacing

--- test/app/models/audio.rb
+++ test/app/models/audio.rb
@@ -1,11 +1,11 @@
 class Audio < ActiveRecord::Base
   
   has_attached :file, :processor => :audio, :path => "audios/files/:style/:identifier:extension", 
-  :styles => {
-    :full  => { :preset => "320kbps", :extension => ".wav" },
-    :large => { :preset => "256kbps", :extension => ".wav" },
-    :small => { :preset => "128kbps", :extension => ".wav" },
-  }
+    :styles => {
+      :full  => { :preset => "320kbps", :extension => ".wav" },
+      :large => { :preset => "256kbps", :extension => ".wav" },
+      :small => { :preset => "128kbps", :extension => ".wav" },
+    }
   
   validates_attached_presence :file
   validates_attached_size :file, :in => 2.kilobytes..2.megabytes
--- test/app/models/image.rb
+++ test/app/models/image.rb
@@ -1,12 +1,12 @@
 class Image < ActiveRecord::Base
   
   has_attached :file, :processor => :image, :path => "images/files/:style/:identifier:extension", 
-  :styles => {
-    :full  => { :size => "400x400#", :extension => ".jpg", :quality => 90 },
-    :large => { :size => "400x400<", :extension => ".jpg", :quality => 90 },
-    :small => { :size => "200x200>", :extension => ".jpg", :quality => 90 },
-    :thumb => { :size => "100x100#", :extension => ".jpg", :quality => 90 },
-  }
+    :styles => {
+      :full  => { :size => "400x400#", :extension => ".jpg", :quality => 90 },
+      :large => { :size => "400x400<", :extension => ".jpg", :quality => 90 },
+      :small => { :size => "200x200>", :extension => ".jpg", :quality => 90 },
+      :thumb => { :size => "100x100#", :extension => ".jpg", :quality => 90 },
+    }
   
   validates_attached_presence :file
   validates_attached_size :file, :in => 2.kilobytes..2.megabytes
