a9927359c8cfcf90529c17392d1a64ea0e434a49
Kevin Sylvestre <kevin@ksylvest.com>
2011-05-15 14:05:57 -0400

    updated attachment options

--- test/app/models/image.rb
+++ test/app/models/image.rb
@@ -2,10 +2,10 @@ class Image < ActiveRecord::Base
   
   has_attached :file, :processor => :image, :path => "images/files/:style/:identifier:extension", 
   :styles => {
-    :full  => { :size => "400x400#", :extension => ".jpg" },
-    :thumb => { :size => "100x100#", :extension => ".jpg" },
-    :small => { :size => "200x200>", :extension => ".jpg" },
-    :large => { :size => "400x400<", :extension => ".jpg" },
+    :full  => { :size => "400x400#", :extension => ".jpg", :quality => 90 },
+    :large => { :size => "400x400<", :extension => ".jpg", :quality => 90 },
+    :small => { :size => "200x200>", :extension => ".jpg", :quality => 90 },
+    :thumb => { :size => "100x100#", :extension => ".jpg", :quality => 90 },
   }
   
   validates_attached_presence :file
