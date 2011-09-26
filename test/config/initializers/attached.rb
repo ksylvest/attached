ba40e6371fda794a936f7c8218e9e0c1fa28ed28
Kevin Sylvestre <kevin@ksylvest.com>
2011-09-26 00:18:22 -0700

    cleaned up environment initializer

--- test/config/initializers/attached.rb
+++ test/config/initializers/attached.rb
@@ -1,11 +1,4 @@
-case ENV['ATTACHED']
-when "aws"
-  Attached::Attachment.options[:medium] = :aws
-  Attached::Attachment.options[:credentials] = "#{Rails.root}/config/aws.yml"
-when "google"
-  Attached::Attachment.options[:medium] = :google
-  Attached::Attachment.options[:credentials] = "#{Rails.root}/config/google.yml"
-when "rackspace"
-  Attached::Attachment.options[:medium] = :rackspace
-  Attached::Attachment.options[:credentials] = "#{Rails.root}/config/rackspace.yml"
-end
+environment = ENV['ATTACHED']
+
+Attached::Attachment.options[:medium] = environment.intern if environment
+Attached::Attachment.options[:credentials] = "#{Rails.root}/config/#{environment}.yml" if environment
