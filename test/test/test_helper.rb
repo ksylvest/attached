5a6b48dd9b15e4bba7ec5acca681db1fa4e6c50d
Kevin Sylvestre <kevin@ksylvest.com>
2011-09-26 00:19:54 -0700

    added code coverage

--- test/test/test_helper.rb
+++ test/test/test_helper.rb
@@ -1,9 +1,11 @@
+require 'mocha'
+require 'simplecov'
+SimpleCov.start if ENV["COVERAGE"]
+
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  
   fixtures :all
-  
 end
