b3aef5ff44a23ea8f76ba519ff849a4b5a5412e6
Kevin Sylvestre <kevin@ksylvest.com>
2011-09-26 00:20:34 -0700

    added encoding and status

--- test/db/schema.rb
+++ test/db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -16,7 +17,6 @@ ActiveRecord::Schema.define(:version => 20101223191846) do
     t.string   "name"
     t.string   "file_identifier"
     t.string   "file_extension"
-    t.string   "file_status"
     t.integer  "file_size"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(:version => 20101223191846) do
     t.string   "name"
     t.string   "file_identifier"
     t.string   "file_extension"
-    t.string   "file_status"
     t.integer  "file_size"
     t.datetime "created_at"
     t.datetime "updated_at"
