510c1723954411a71147af13fb783cd4d1cb8b2c
Kevin Sylvestre <kevin@ksylvest.com>
2011-09-26 00:18:40 -0700

    cleaned up controllers in tests to use new syntax

--- test/app/controllers/audios_controller.rb
+++ test/app/controllers/audios_controller.rb
@@ -1,85 +1,57 @@
 class AudiosController < ApplicationController
   
+  respond_to :html
+  
   # GET /audios
-  # GET /audios.xml
   def index
     @audios = Audio.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @audios }
-    end
+    respond_with(@audios)
   end
 
   # GET /audios/1
-  # GET /audios/1.xml
   def show
     @audio = Audio.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @audio }
-    end
+    respond_with(@audio)
   end
 
   # GET /audios/new
-  # GET /audios/new.xml
   def new
     @audio = Audio.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @audio }
-    end
+    respond_with(@audio)
   end
 
   # GET /audios/1/edit
   def edit
     @audio = Audio.find(params[:id])
+    
+    respond_with(@audio)
   end
 
   # POST /audios
-  # POST /audios.xml
   def create
-    @audio = Audio.new(params[:audio])
+    @audio = Audio.create(params[:audio])
 
-    respond_to do |format|
-      if @audio.save
-        format.html { redirect_to(@audio, :notice => 'Audio was successfully created.') }
-        format.xml  { render :xml => @audio, :status => :created, :location => @audio }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @audio.errors, :status => :unprocessable_entity }
-      end
-    end
+    respond_with(@audio)
   end
 
   # PUT /audios/1
-  # PUT /audios/1.xml
   def update
     @audio = Audio.find(params[:id])
-
-    respond_to do |format|
-      if @audio.update_attributes(params[:audio])
-        format.html { redirect_to(@audio, :notice => 'Audio was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @audio.errors, :status => :unprocessable_entity }
-      end
-    end
+    @audio.attributes = params[:audio]
+    @audio.save
+    
+    respond_with(@audio)
   end
 
   # DELETE /audios/1
-  # DELETE /audios/1.xml
   def destroy
     @audio = Audio.find(params[:id])
     @audio.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(audios_url) }
-      format.xml  { head :ok }
-    end
+    respond_with(@audio)
   end
   
 end
--- test/app/controllers/images_controller.rb
+++ test/app/controllers/images_controller.rb
@@ -1,85 +1,56 @@
 class ImagesController < ApplicationController
   
+  respond_to :html
+  
   # GET /images
-  # GET /images.xml
   def index
     @images = Image.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @images }
-    end
+    respond_with(@images)
   end
 
   # GET /images/1
-  # GET /images/1.xml
   def show
     @image = Image.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @image }
-    end
+    respond_with(@image)
   end
 
   # GET /images/new
-  # GET /images/new.xml
   def new
     @image = Image.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @image }
-    end
+    respond_with(@image)
   end
 
   # GET /images/1/edit
   def edit
     @image = Image.find(params[:id])
+
+    respond_with(@image)
   end
 
   # POST /images
-  # POST /images.xml
   def create
-    @image = Image.new(params[:image])
+    @image = Image.create(params[:image])
+    @image.save
 
-    respond_to do |format|
-      if @image.save
-        format.html { redirect_to(@image, :notice => 'Image was successfully created.') }
-        format.xml  { render :xml => @image, :status => :created, :location => @image }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @image.errors, :status => :unprocessable_entity }
-      end
-    end
+    respond_with(@image)
   end
 
   # PUT /images/1
-  # PUT /images/1.xml
   def update
     @image = Image.find(params[:id])
 
-    respond_to do |format|
-      if @image.update_attributes(params[:image])
-        format.html { redirect_to(@image, :notice => 'Image was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @image.errors, :status => :unprocessable_entity }
-      end
-    end
+    respond_with(@image)
   end
 
   # DELETE /images/1
-  # DELETE /images/1.xml
   def destroy
     @image = Image.find(params[:id])
     @image.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(images_url) }
-      format.xml  { head :ok }
-    end
+    respond_with(@image)
   end
   
 end
