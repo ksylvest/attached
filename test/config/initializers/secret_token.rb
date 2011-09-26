12ae00f28bb22f0f41540c7e3b35742555b58e66
Kevin Sylvestre <kevin@ksylvest.com>
2011-09-25 23:34:59 -0700

    consolidated initializers

--- test/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Attached::Application.config.secret_token = '75df5e9fde8533af395cff5bdfc330673a5d3e86b9749a1a607cca493c2c3ab543b1a1b4372dbc57f26a47e9eb8faafc09b90a621264b47e83bd2d7d9687c109'
