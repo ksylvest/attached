a1384d7957fde90a6f8f81dd6cb3faf47fdbbd06
Kevin Sylvestre <kevin@ksylvest.com>
2013-05-25 18:41:04 -0700

    updates

--- test/dummy/config/initializers/attached.rb
+++ test/dummy/config/initializers/attached.rb
@@ -1,4 +1,4 @@
-environment = ENV['ATTACHED']
+environment = ENV['ATTACHED'] || 'aws'
 
 Attached::Attachment.options[:medium] = environment.intern if environment
 Attached::Attachment.options[:credentials] = "#{Rails.root}/config/#{environment}.yml" if environment
